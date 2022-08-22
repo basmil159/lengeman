0c62daab18433be33d85357df8d280f6b726e28a
basmil159 <basmilbhan@gmail.com>
2022-08-22 12:49:28 +0545

    gitignore

--- src/components/Svgs.jsx
+++ src/components/Svgs.jsx
@@ -1,7 +1,7 @@
 export const Svgs = (props) => {
     return (
         <svg class={props.title}>
-                  <use xlinkHref={`img/blackbox.svg#${props.icon}`}></use>
+                  <use xlinkHref={process.env.PUBLIC_URL + `img/blackbox.svg#${props.icon}`}></use>
                 </svg>
                 );
 };
