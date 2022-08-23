a5c791f145a7c5a41dbf9673498df8144a0772d7
basmil159 <basmilbhan@gmail.com>
2022-08-23 13:53:00 +0545

    active state 2.o

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ export const App = () => {
           <div className="wrapper__body">
           <Routes>
             <Route path="lengeman" element={<Jobs />} />
-            {/* <Route path="tools" element={<Tools />} /> */}
-            {/* <Route path="materials" element={<Materials />} /> */}
-            {/* <Route path="setting" element={<Setting />} /> */}
-            {/* <Route path="logs" element={<Logs />} /> */}
+            <Route path="tools" element={<Tools />} />
+            <Route path="materials" element={<Materials />} />
+            <Route path="setting" element={<Setting />} />
+            <Route path="logs" element={<Logs />} />
           </Routes>
          </div>
          </>
@@ -23,3 +23,8 @@ export const App = () => {
 };
 
 
+let Tools, Logs, Setting, Materials;
+
+Tools =Logs= Setting = Materials = () => {
+  return <h1 Style="margin:0 auto; color:red;">Page comming soon</h1>;
+};
