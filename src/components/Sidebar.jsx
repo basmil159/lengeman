a47ed4d0b8a9afd07b859605496cfac5f7105e9b
basmil159 <basmilbhan@gmail.com>
2022-08-23 09:51:02 +0545

    tuesday

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,41 +6,69 @@ import logo from '../img/blackbox.png';
  * @NavLinks receives two props, title and icon
  * using template literal, we concatenate props.icon with svg's link to retrieve desired icon
  */
+
+const activePage=()=>{
+  const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.navigation__nav--elements').forEach(link => {
+  link.classList.remove('active');
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+    console.log(link.href.includes(`${activePage}`));
+  }
+})
+};
+
 const NavLinks = (props) => { 
-  // const title=props.title;
+  const title=props.title;
   
+  if (title=="Site Setting") {
     return (
-      <li className="heading-secondary nav-element">
+    <li className="heading-secondary nav-element" >
+       <a className="navigation__nav--elements" href="javascript:void(0);" >
+           <Svgs icon={props.icon} title={`small-svg ${props.title}`} />
+           {props.state && <span className="navigation__nav--hide-on-tab ">{props.title}</span>}
+         </a>
+       </li>
+    );
+  } 
+    return (
+      <li className="heading-secondary nav-element" onClick={activePage}>
        <Link className="navigation__nav--elements" to={props.NavLinks}>
            <Svgs icon={props.icon} title={`small-svg ${props.title}`} />
            {props.state && <span className="navigation__nav--hide-on-tab ">{props.title}</span>}
          </Link>
        </li>
      );
-  };
-
-
-
-export const Sidebar = () => {
+    
+    
+    };
+    
+    
+    
+    export const Sidebar = () => {
   const [openNav, setOpenNav] = useState(false);
   const toggleMenu = () => {
     document.querySelector('.toggle').classList.toggle('active');
     document.getElementById("logo").toggleAttribute("collapse");
+    document.querySelectorAll(".logo-hide").forEach(element => {
+      element.toggleAttribute("hide");
+    });
     if (openNav) {
       setOpenNav(false);
       document.getElementById("sidebar").style.width = "8rem";
       document.querySelectorAll(".nav-element").forEach(element => {
         element.style.justifyContent="center";
       });
+
       
     } else {
       setOpenNav(true);      
       document.getElementById("sidebar").style.width = "20rem"; 
-      // document.querySelector(".nav-element").style.cssText='justifyContent: left; width: calc(100% - 0rem )';
       document.querySelectorAll(".nav-element").forEach(element => {
         element.style.justifyContent="left";
         element.style.width="calc(100% - 0rem )";
       });
+
       document.querySelectorAll(".wrapper__body").forEach(element => {
         element.style.marginLeft="2rem";
       });
@@ -52,10 +80,10 @@ export const Sidebar = () => {
       
       <a href="/lengeman" className="sidebar__logo-box">
         
-      <h1 className="sidebar__logo" id="logo" collapse="1">Lengeman</h1>
-      <div className="sidebar__logo--blackbox">
+      <h1 className="sidebar__logo logo-hide"  hide="1" id="logo" collapse="1">Lengeman</h1>
+      <div className="sidebar__logo--blackbox logo-hide" hide="1">
         <img src={logo} width="28" height="28"  alt="BLACKBOX"/>
-        <span>BlackBOX</span>
+        <span >BlackBOX</span>
       </div>
       
       </a>
@@ -71,8 +99,8 @@ export const Sidebar = () => {
           <NavLinks icon="materials" title="Materials" NavLinks="/Materials" state={openNav}/>
           <NavLinks icon="setting" title="Setting" NavLinks="/Setting" state={openNav}/>
           <NavLinks icon="log" title="Logs" NavLinks="/Logs" state={openNav}/>
-          <NavLinks icon="simulation" title="Simulation" NavLinks="/Logs" state={openNav}/>
-          <NavLinks icon="collapse" title="Site Setting" NavLinks="#" state={openNav}/>
+          <NavLinks icon="simulation" title="Simulation" NavLinks="/simulation" state={openNav}/>
+          <NavLinks icon="setting" title="Site Setting" NavLinks="#" state={openNav}/>
         </ul>
         
         
