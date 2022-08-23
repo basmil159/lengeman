f450ee545bd6ccd17f03f4e8f05cde07362f6870
basmil159 <basmilbhan@gmail.com>
2022-08-23 13:35:07 +0545

    tuesday 2nd load

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,6 +18,16 @@ const navLinks = document.querySelectorAll('.navigation__nav--elements').forEach
 })
 };
 
+
+window.onload=() => {
+  // Call active page function so that the home page or any active page is displayed as active in navbar 
+  activePage();
+  // For extracting the first letter from the username and use it as a profile Pic
+  let firstName = document.getElementById("userName").innerText;
+  let intials = firstName.charAt(0);
+  let profileImage = document.getElementById("profilePic").innerText = intials;
+};
+
 const NavLinks = (props) => { 
   const title=props.title;
   
@@ -32,8 +42,9 @@ const NavLinks = (props) => {
     );
   } 
     return (
+      
       <li className="heading-secondary nav-element" onClick={activePage}>
-       <Link className="navigation__nav--elements" to={props.NavLinks}>
+       <Link className={`navigation__nav--elements`} to={props.NavLinks}>
            <Svgs icon={props.icon} title={`small-svg ${props.title}`} />
            {props.state && <span className="navigation__nav--hide-on-tab ">{props.title}</span>}
          </Link>
@@ -46,65 +57,83 @@ const NavLinks = (props) => {
     
     
     export const Sidebar = () => {
-  const [openNav, setOpenNav] = useState(false);
-  const toggleMenu = () => {
-    document.querySelector('.toggle').classList.toggle('active');
-    document.getElementById("logo").toggleAttribute("collapse");
-    document.querySelectorAll(".logo-hide").forEach(element => {
-      element.toggleAttribute("hide");
-    });
-    if (openNav) {
-      setOpenNav(false);
-      document.getElementById("sidebar").style.width = "8rem";
-      document.querySelectorAll(".nav-element").forEach(element => {
-        element.style.justifyContent="center";
-      });
+      const [openNav, setOpenNav] = useState(false);
 
       
-    } else {
-      setOpenNav(true);      
-      document.getElementById("sidebar").style.width = "20rem"; 
-      document.querySelectorAll(".nav-element").forEach(element => {
-        element.style.justifyContent="left";
-        element.style.width="calc(100% - 0rem )";
-      });
 
-      document.querySelectorAll(".wrapper__body").forEach(element => {
-        element.style.marginLeft="2rem";
-      });
-  
-    }
-  };
-  return (
-    <section id="sidebar" className="sidebar">
       
-      <a href="/lengeman" className="sidebar__logo-box">
-        
-      <h1 className="sidebar__logo logo-hide"  hide="1" id="logo" collapse="1">Lengeman</h1>
-      <div className="sidebar__logo--blackbox logo-hide" hide="1">
-        <img src={logo} width="28" height="28"  alt="BLACKBOX"/>
-        <span >BlackBOX</span>
-      </div>
+      const toggleMenu = () => {
+        document.querySelector('.toggle').classList.toggle('active');
+        document.getElementById("logo").toggleAttribute("collapse");
+        document.querySelectorAll(".logo-hide").forEach(element => {
+          element.toggleAttribute("hide");
+        });
+        if (openNav) {
+          setOpenNav(false);
+          document.getElementById("sidebar").style.width = "8rem";
+          document.querySelectorAll(".nav-element").forEach(element => {
+            element.style.justifyContent="center";
+          });
+
+          
+        } else {
+          setOpenNav(true);      
+          document.getElementById("sidebar").style.width = "20rem"; 
+          document.querySelectorAll(".nav-element").forEach(element => {
+            element.style.justifyContent="left";
+            element.style.width="calc(100% - 0rem )";
+          });
+
+          document.querySelectorAll(".wrapper__body").forEach(element => {
+            element.style.marginLeft="2rem";
+          });
       
-      </a>
-      <div class="toggle" onClick={toggleMenu}>
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-      <div id="navigation" className="navigation">
-        <ul className="navigation__nav">
-          <NavLinks icon="work" title="Jobs" NavLinks="/lengeman" state={openNav}/>
-          <NavLinks icon="tool" title="Tool" NavLinks="/tools" state={openNav}/>
-          <NavLinks icon="materials" title="Materials" NavLinks="/Materials" state={openNav}/>
-          <NavLinks icon="setting" title="Setting" NavLinks="/Setting" state={openNav}/>
-          <NavLinks icon="log" title="Logs" NavLinks="/Logs" state={openNav}/>
-          <NavLinks icon="simulation" title="Simulation" NavLinks="/simulation" state={openNav}/>
-          <NavLinks icon="setting" title="Site Setting" NavLinks="#" state={openNav}/>
-        </ul>
-        
-        
-      </div>
-    </section>
-  );
+        }
+      };
+
+     
+
+      
+      
+
+      return (
+        <section id="sidebar" className="sidebar">          
+          <a href="/lengeman" className="sidebar__logo-box">            
+            <h1 className="sidebar__logo logo-hide"  hide="1" id="logo" collapse="1">Lengeman</h1>
+            <div className="sidebar__logo--blackbox logo-hide" hide="1">
+              <img src={logo} width="28" height="28"  alt="BLACKBOX"/>
+              <span >BlackBOX</span>
+            </div>
+          </a>
+          
+          <div class="toggle" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+          <div id="navigation" className="navigation">
+            <ul className="navigation__nav">
+              
+              <NavLinks icon="work" title="Jobs" NavLinks="/lengeman" state={openNav}/>
+              <NavLinks icon="tool" title="Tool" NavLinks="/tools" state={openNav}/>
+              <NavLinks icon="materials" title="Materials" NavLinks="/Materials" state={openNav}/>
+              <NavLinks icon="setting" title="Setting" NavLinks="/Setting" state={openNav}/>
+              <NavLinks icon="log" title="Logs" NavLinks="/Logs" state={openNav}/>
+              <NavLinks icon="simulation" title="Simulation" NavLinks="/simulation" state={openNav}/>
+              {/* <NavLinks icon="setting" title="Site Setting" NavLinks="#" state={openNav}/> */}
+            </ul>
+          </div>
+          <div className="sidebar__bottom">
+            <div className="sidebar__user logo-hide" hide="1">
+              <div className="sidebar__user--pic" id="profilePic"></div>
+              <span className="sidebar__user--name " id="userName"  >Harry</span>
+            </div>
+            <div className="sidebar__bottom--divider"></div>
+            <div className="sidebar__setting logo-hide" hide="1">
+            <Svgs icon="setting" title="small-svg" />
+           <span>Setting</span>
+            </div> 
+          </div>
+        </section>
+      );
 };
